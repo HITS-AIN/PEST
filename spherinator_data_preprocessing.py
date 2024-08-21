31c49ea5f37d6505c407bf33bb7025b5ac268f96
sebastian-tg <strujill@gmail.com>
2024-08-21 12:09:17 +0200

    Updated tests

--- spherinator_data_preprocessing.py
+++ spherinator_data_preprocessing.py
@@ -235,8 +235,6 @@ def get(path, params=None):
 
 
 ### Main routine to read data using API
-
-
 def data_preprocess_api(
     sim="TNG100-1",
     snapshot=99,
@@ -304,7 +302,7 @@ def data_preprocess_api(
         f" operation: {operation}\n"
         f" fov: {fov}\n"
         f" image_depth: {image_depth} particles\n"
-        f" image_size: {image_size}\n"
+        f" image_size: {image_size} pixels\n"
         f" smoothing: {smoothing} kpc\n"
         f" channels: {channels}\n"
         f" image_scale: {image_scale}\n"
@@ -532,7 +530,7 @@ def data_preprocess_api(
 # To test routine run:
 #
 # result = data_preprocess_api(
-#                 sim='TNG50-1',
+#                 sim='TNG50-2',
 #                 selection_type='stellar mass',
 #                 min_mass=5e10, max_mass=5.2e10, # [Msun]
 #                 component='stars',
@@ -547,33 +545,31 @@ def data_preprocess_api(
 #                 output_path='./images_test_api/',
 #                 debug=False)
 #
-# result:
+# result should be exactly:
 #
-# {'SubID': [480802, 510585, 535774, 540082, 547844],
-#  'GroupID': [117, 162, 212, 222, 244],
-#  'logMstar': array([10.70407271, 10.71019709, 10.71117686, 10.71391965, 10.70766696]),
-#  'logMtot': array([12.15007401, 12.14745496, 11.99690134, 11.99601387, 11.99329041]),
-#  'logMhalo': array([12.32121876, 12.11980212, 12.00309208, 11.98022773, 11.98260985]),
-#  'Rhalf': [7.438145851786241,
-#   5.3811632713315625,
-#   5.2503690581635665,
-#   4.100236197224683,
-#   5.35016238559197],
-#  'SubhaloStarMetallicity': [0.024397047236561775,
-#   0.024507567286491394,
-#   0.02150518074631691,
-#   0.025892818346619606,
-#   0.025879880413413048],
-#  'SubhaloSFR': [4.6734747886657715,
-#   3.11273455619812,
-#   9.466231346130371,
-#   0.0,
-#   1.6749541759490967]}
+# {'SubID': [79417, 79580, 79811, 83918, 86024],
+# 'GroupID': [99, 100, 102, 131, 149],
+# 'logMstar': array([10.70847058, 10.710352  , 10.7067737 , 10.70629613, 10.6992251 ]),
+# 'logMtot': array([12.27970075, 12.2991249 , 12.42821564, 12.25034364, 12.11282093]),
+# 'logMhalo': array([12.22113751, 12.31243984, 12.3679076 , 12.17308883, 12.03844093]),
+# 'Rhalf': [2.9622084440507828,
+#  5.76645999409507,
+#  3.437998228520815,
+#  4.131237082964276,
+#  5.301299084735755],
+# 'SubhaloStarMetallicity': [0.024265184998512268,
+#  0.022633448243141174,
+#  0.02354022115468979,
+#  0.024240750819444656,
+#  0.023321228101849556],
+# 'SubhaloSFR': [0.0,
+#  0.32513657212257385,
+#  0.0,
+#  0.04936574399471283,
+#  4.49891996383667]}
 
 
 ### Read data using local files
-
-
 def data_preprocess_local(
     sim="TNG100-1",
     snapshot=99,
@@ -644,7 +640,7 @@ def data_preprocess_local(
         f" operation: {operation}\n"
         f" fov: {fov}\n"
         f" image_depth: {image_depth} particles\n"
-        f" image_size: {image_size}\n"
+        f" image_size: {image_size} pixels\n"
         f" smoothing: {smoothing} kpc\n"
         f" channels: {channels}\n"
         f" image_scale: {image_scale}\n"
@@ -873,19 +869,19 @@ def data_preprocess_local(
 #
 # result:
 #
-# {'SubID': [480802, 510585, 535774, 540082, 547844],
-#  'GroupID': [117, 162, 212, 222, 244],
-#  'logMstar': array([10.70407203, 10.71019717, 10.71117675, 10.71391985, 10.7076665 ]),
-#  'logMtot': array([12.15007404, 12.14745474, 11.99690144, 11.99601359, 11.99329026]),
-#  'logMhalo': array([12.32121876, 12.11980212, 12.00309208, 11.98022773, 11.98260985]),
-#  'Rhalf': [7.438102865205036,
-#   5.3811287999610675,
-#   5.250432549728063,
-#   4.100286752944643,
-#   5.350142479787577],
-#  'SubhaloStarMetallicity': [0.024397047,
-#   0.024507567,
-#   0.02150518,
-#   0.025892818,
-#   0.02587988],
-#  'SubhaloSFR': [4.673475, 3.1127346, 9.466231, 0.0, 1.6749542]}
+# {'SubID': [79417, 79580, 79811, 83918, 86024],
+#  'GroupID': [99, 100, 102, 131, 149],
+#  'logMstar': array([10.7084704 , 10.71035241, 10.70677389, 10.70629614, 10.69922539]),
+#  'logMtot': array([12.27970013, 12.29912528, 12.42821589, 12.25034259, 12.11282079]),
+#  'logMhalo': array([12.22113751, 12.31243984, 12.3679076 , 12.17308883, 12.03844093]),
+#  'Rhalf': [2.9622568716719884,
+#   5.76649192233405,
+#   3.4379403007604177,
+#   4.131219575001772,
+#   5.301296235820272],
+#  'SubhaloStarMetallicity': [0.024265185,
+#   0.022633448,
+#   0.023540221,
+#   0.02424075,
+#   0.023321228],
+#  'SubhaloSFR': [0.0, 0.32513657, 0.0, 0.049365744, 4.49892]}
