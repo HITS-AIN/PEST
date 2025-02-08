82fb37e11190a421f81980d18dae1b255fccb53b
Bernd Doser <bernd.doser@h-its.org>
2025-02-08 14:12:06 +0100

    fix black format

--- src/pest/gaia_calibrate.py
+++ src/pest/gaia_calibrate.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
-""" Calibrate Gaia XP continuous to spectra and store to a Parquet file.
-"""
+"""Calibrate Gaia XP continuous to spectra and store to a Parquet file."""
 
 import os
 from multiprocessing import Pool
