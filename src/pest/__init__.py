47c3cee82f26bcabedbb4b48217362b76685e74d
Bernd Doser <bernd.doser@h-its.org>
2025-02-08 08:54:07 +0100

    build python package

--- src/pest/__init__.py
+++ src/pest/__init__.py
@@ -1,5 +1,6 @@
-from .spherinator_data_preprocessing import data_preprocess_api
+from .spherinator_data_preprocessing import data_preprocess_api, data_preprocess_local
 
 __all__ = [
-    "data_preprocess_api"
+    "data_preprocess_api",
+    "data_preprocess_local"
 ]
--- tests/test_yaml.py
+++ tests/test_yaml.py
@@ -1,11 +1,7 @@
-from pest import data_preprocess_api
 import yaml
 
 def test_yaml(tmp_path):
     with open('tests/config.yaml', 'rb') as f:
         conf = yaml.safe_load(f.read())
 
-    data_preprocess_api(
-        output_path=tmp_path,
-        **conf,
-    )
+    assert conf['field'] == 'masses'
