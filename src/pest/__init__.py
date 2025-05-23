c118418dfda9404858d38c7083b7c2060d954244
Bernd Doser <bernd.doser@h-its.org>
2025-05-23 19:20:21 +0200

    Add version string for the pest package

--- src/pest/__init__.py
+++ src/pest/__init__.py
@@ -1,7 +1,10 @@
+import importlib.metadata
+
 from .fits_converter import FitsConverter
 from .gaia_converter import GaiaConverter
 from .spherinator_data_preprocessing import data_preprocess_api, data_preprocess_local
 
+__version__ = importlib.metadata.version("pest")
 __all__ = [
     "data_preprocess_api",
     "data_preprocess_local",
