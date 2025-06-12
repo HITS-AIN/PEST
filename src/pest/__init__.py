db4bad3d41bfcb3abdb27900979dbe448fb78f2c
Bernd Doser <bernd.doser@h-its.org>
2025-06-12 15:23:53 +0200

    update package version reference to astro-pest

--- src/pest/__init__.py
+++ src/pest/__init__.py
@@ -3,7 +3,7 @@ import importlib.metadata
 from .fits_converter import FitsConverter
 from .gaia_converter import GaiaConverter
 
-__version__ = importlib.metadata.version("pest")
+__version__ = importlib.metadata.version("astro-pest")
 __all__ = [
     "GaiaConverter",
     "FitsConverter",
